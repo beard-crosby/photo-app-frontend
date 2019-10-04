7367ad65ee9bf4fde8ad8db1010a1a44af7a2c92
Maximilian118 <maxcrosby118@gmail.com>
2019-10-05 00:49:39 +0100

    Settings Start

--- src/Router.js
+++ src/Router.js
@@ -4,11 +4,13 @@ import { Route, Switch } from 'react-router-dom'
 import Notfound from './pages/NotFound'
 import Profile from './pages/Profile'
 import Following from './pages/Following'
+import Settings from './pages/Settings'
 
 const Router = () =>
   <Switch>
     <Route exact path="/" component={Following}/>
     <Route path="/profile" component={Profile}/>
+    <Route path="/settings" component={Settings}/>
     <Route component={Notfound}/>
   </Switch>
 
---
+++ src/pages/Settings.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../scss/settings.scss'
+import Button from '../components/UI/Button'
+
+const Settings = () =>
+  <div className="SettingsWrapper">
+    <Button text="Logout"/>
+  </div>
+
+
+export default Settings
