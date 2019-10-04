aeae5d2eb0815413e45f9b1cb1e8cd29dcc2382d
Maximilian118 <maxcrosby118@gmail.com>
2019-10-05 00:18:25 +0100

    Simple Following Page

--- src/components/PhotoCard/PhotoCard.js
+++ src/components/PhotoCard/PhotoCard.js
@@ -1,21 +1,23 @@
 import React from 'react'
 import * as classes from './PhotoCard.module.scss'
 
-const PhotoCard = ({ img, style }) => 
+const PhotoCard = ({ img, name, username, profileImg, style }) => 
   <div className={classes.PhotoCardWrapper} style={style}>
     <div className={classes.ImgWrapper}>
       {img}
     </div>
     <div className={classes.Sidebar}>
       <div className={classes.Creator}>
-        <div className={classes.ProfilePicture}/>
+        <div className={classes.ProfilePicture}>
+          {profileImg}
+        </div>
         <div className={classes.ProfileInfo}>
-          <h5>Maximilian Crosby</h5>
-          <p>Maxiscoolerthansam92</p>
+          <h5>{name}</h5>
+          <p>{username}</p>
         </div>
       </div>
       <div className={classes.Comments}>
-
+        <input type="text" name="comment" id="comment" placeholder="Write a comment" />
       </div>
     </div>
   </div>
--- src/pages/Following.js
+++ src/pages/Following.js
@@ -4,11 +4,35 @@ import PhotoCard from '../components/PhotoCard'
 
 const Following = () => 
   <div className="Following-Wrapper">
-    <PhotoCard img={<img alt="Test Img" src={require('../static/testPictures/8.jpg')}/>} style={{ marginBottom: 40 }}/>
-    <PhotoCard img={<img alt="Test Img" src={require('../static/testPictures/9.jpg')}/>} style={{ marginBottom: 40 }}/>
-    <PhotoCard img={<img alt="Test Img" src={require('../static/testPictures/3.jpg')}/>} style={{ marginBottom: 40 }}/>
-    <PhotoCard img={<img alt="Test Img" src={require('../static/testPictures/7.jpg')}/>} style={{ marginBottom: 40 }}/>
-    <PhotoCard img={<img alt="Test Img" src={require('../static/testPictures/11.jpg')}/>}/>
+    <PhotoCard 
+      img={<img alt="Test Img" src={require('../static/testPictures/8.jpg')}/>}
+      name="Maximilian Crosby"
+      username="Samissupergay69"
+      profileImg={<img alt="Test Img" src={require('../static/defaults/placeholder.png')}/>}
+      style={{ marginBottom: 40 }}/>
+    <PhotoCard 
+      img={<img alt="Test Img" src={require('../static/testPictures/9.jpg')}/>} 
+      name="Samuel Beard"
+      username="TestyMctesttest92"
+      profileImg={<img alt="Test Img" src={require('../static/defaults/placeholder.png')}/>}
+      style={{ marginBottom: 40 }}/>
+    <PhotoCard 
+      img={<img alt="Test Img" src={require('../static/testPictures/3.jpg')}/>} 
+      name="I feel the need"
+      username="The_need_for_speed23"
+      profileImg={<img alt="Test Img" src={require('../static/defaults/placeholder.png')}/>}
+      style={{ marginBottom: 40 }}/>
+    <PhotoCard 
+      img={<img alt="Test Img" src={require('../static/testPictures/7.jpg')}/>} 
+      name="Stupidly Long and Convoluted Name"
+      username="If_You_wAnT_tO_Live"
+      profileImg={<img alt="Test Img" src={require('../static/defaults/placeholder.png')}/>}
+      style={{ marginBottom: 40 }}/>
+    <PhotoCard 
+      img={<img alt="Test Img" src={require('../static/testPictures/11.jpg')}/>}
+      name="Im Gonna Get Them"
+      username="If_Its_The_Last_Thing_I_do"
+      profileImg={<img alt="Test Img" src={require('../static/defaults/placeholder.png')}/>}/>
   </div>
 
 export default Following
