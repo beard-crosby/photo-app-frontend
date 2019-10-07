18cc15fb4bce97c9886bbbf527da8803fe37c0bb
Samuel Beard <sbeard@brightsource.co.uk>
2019-10-07 17:05:58 +0100

    another direction

--- src/components/PhotoCard/PhotoCard.js
+++ src/components/PhotoCard/PhotoCard.js
@@ -8,12 +8,8 @@ const PhotoCard = ({ img, name, profileImg, style }) =>
     </div>
     <div className={classes.Sidebar}>
       <div className={classes.Creator}>
-        <div className={classes.ProfilePicture}>
           {profileImg}
-        </div>
-        <div className={classes.ProfileInfo}>
           <h5>{name}</h5>
-        </div>
       </div>
       <div className={classes.Comments}>
         <input type="text" name="comment" id="comment" placeholder="Write a comment" />
