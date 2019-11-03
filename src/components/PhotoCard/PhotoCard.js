815b51d49dd5e885f2e5a05b31274f7309cc2d0f
Maximilian Crosby <maximilian@maximilians-mbp.home>
2019-11-03 02:55:42 +0000

    Links and PhotoCard Style

--- src/components/PhotoCard/PhotoCard.js
+++ src/components/PhotoCard/PhotoCard.js
@@ -1,25 +1,40 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import * as classes from './PhotoCard.module.scss'
 
-const PhotoCard = ({ img, name, username, profileImg, style }) => 
-  <div className={classes.PhotoCardWrapper} style={style}>
-    <div className={classes.ImgWrapper}>
-      {img}
-    </div>
-    <div className={classes.Sidebar}>
-      <div className={classes.Creator}>
-        <div className={classes.ProfilePicture}>
-          {profileImg}
-        </div>
-        <div className={classes.ProfileInfo}>
-          <h5>{name}</h5>
-          <p>{username}</p>
-        </div>
+const PhotoCard = ({ ident, img, name, profileImg, style }) => {
+  const [height, setHeight] = useState(null)
+  useEffect(() => setHeight(document.getElementsByClassName('PhotoCard_ImgWrapper__1IPFj').item(ident).clientHeight), [])
+
+  const creatorJSX = (
+    <>
+      <div className={classes.ProfilePicture}>
+        {profileImg}
       </div>
-      <div className={classes.Comments}>
+      <div className={classes.ProfileInfo}>
+        <h5>{name}</h5>
+      </div>
+    </>
+  )
+
+  return (
+    <div className={classes.PhotoCardWrapper} style={style}>
+      <div className={classes.CreatorMobile}>
+        {creatorJSX}
+      </div>
+      <div className={classes.ImgWrapper}>
+        {img}
+      </div>
+      <div className={classes.Sidebar} style={window.matchMedia("(min-width: 600px)").matches ? { height: height } : null}>
+        <div className={classes.SidebarTop}>
+          <div className={classes.Creator}>
+            {creatorJSX}
+          </div>
+          <div className={classes.Comments}/>
+        </div>
         <input type="text" name="comment" id="comment" placeholder="Write a comment" />
       </div>
     </div>
-  </div>
+  )
+}
 
 export default PhotoCard
--- src/pages/Following.js
+++ src/pages/Following.js
@@ -1,38 +1,18 @@
 import React from 'react'
 import '../scss/following.scss'
 import PhotoCard from '../components/PhotoCard'
+import testData from '../testData'
 
 const Following = () => 
   <div className="Following-Wrapper">
-    <PhotoCard 
-      img={<img alt="Test Img" src={require('../static/testPictures/8.jpg')}/>}
-      name="Maximilian Crosby"
-      username="Samissupergay69"
-      profileImg={<img alt="Test Img" src={require('../static/defaults/placeholder.png')}/>}
-      style={{ marginBottom: 40 }}/>
-    <PhotoCard 
-      img={<img alt="Test Img" src={require('../static/testPictures/9.jpg')}/>} 
-      name="Samuel Beard"
-      username="TestyMctesttest92"
-      profileImg={<img alt="Test Img" src={require('../static/defaults/placeholder.png')}/>}
-      style={{ marginBottom: 40 }}/>
-    <PhotoCard 
-      img={<img alt="Test Img" src={require('../static/testPictures/3.jpg')}/>} 
-      name="I feel the need"
-      username="The_need_for_speed23"
-      profileImg={<img alt="Test Img" src={require('../static/defaults/placeholder.png')}/>}
-      style={{ marginBottom: 40 }}/>
-    <PhotoCard 
-      img={<img alt="Test Img" src={require('../static/testPictures/7.jpg')}/>} 
-      name="Stupidly Long and Convoluted Name"
-      username="If_You_wAnT_tO_Live"
-      profileImg={<img alt="Test Img" src={require('../static/defaults/placeholder.png')}/>}
-      style={{ marginBottom: 40 }}/>
-    <PhotoCard 
-      img={<img alt="Test Img" src={require('../static/testPictures/11.jpg')}/>}
-      name="Im Gonna Get Them"
-      username="If_Its_The_Last_Thing_I_do"
-      profileImg={<img alt="Test Img" src={require('../static/defaults/placeholder.png')}/>}/>
+    {testData.map((testData, i) => 
+      <PhotoCard
+        key={i}
+        ident={i}
+        img={<img alt="Test Img" src={require(`../static/testPictures/${testData.img}`)}/>}
+        name={testData.name}
+        profileImg={<img alt="Test Img" src={require(`../static/defaults/${testData.profileImg}`)}/>}
+        style={window.matchMedia("(min-width: 600px)").matches ? { marginBottom: 40 } : { marginBottom: 20 }}/>)}
   </div>
 
 export default Following
