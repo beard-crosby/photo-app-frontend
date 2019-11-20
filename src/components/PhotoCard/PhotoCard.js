6a43ead650693c6096b4b697e0868e1bf40c5bb6
Maximilian118 <maxcrosby118@gmail.com>
2019-11-20 22:52:05 +0000

    PhotoCard Img Maximise

--- src/components/PhotoCard/PhotoCard.js
+++ src/components/PhotoCard/PhotoCard.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types'
 
 const PhotoCard = ({ ident, img, name, profileImg }) => {
   const [height, setHeight] = useState(null)
+  const [imgClicked, setImgClicked] = useState(null)
   
   useEffect(() => setHeight(document.getElementsByClassName('_PhotoCard_ImgWrapper__3S6GQ').item(ident).clientHeight), [ident])
 
@@ -18,12 +19,22 @@ const PhotoCard = ({ ident, img, name, profileImg }) => {
     </>
   )
 
+  const imgClickedHandler = () => {
+    if (imgClicked === null) {
+      setImgClicked(classes.imgClicked)
+      document.body.style.overflow = "hidden"
+    } else {
+      setImgClicked(null)
+      document.body.style = "none"
+    }
+  }
+
   return (
-    <div className={classes.PhotoCardWrapper} style={window.matchMedia("(min-width: 600px)").matches ? { marginBottom: 40 } : { marginBottom: 20 }}>
+    <div className={`${classes.PhotoCardWrapper} ${imgClicked}`} style={window.matchMedia("(min-width: 600px)").matches ? { marginBottom: 40 } : { marginBottom: 20 }}>
       <div className={classes.CreatorMobile}>
         {creatorJSX}
       </div>
-      <div className={classes.ImgWrapper}>
+      <div className={classes.ImgWrapper} onClick={() => imgClickedHandler()}>
         {img}
       </div>
       <div className={classes.Sidebar} style={window.matchMedia("(min-width: 600px)").matches ? { height: height } : null}>
