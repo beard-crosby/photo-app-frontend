caa35be3265107bc207d3df98b3ffa8208a138c9
Maximilian118 <maxcrosby118@gmail.com>
2020-03-04 07:26:12 +0000

    NotFound Dark_mode

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { UserContext } from '../App'
 
-const NotFound = () =>
-  <h2>Wups! Page Not Found...</h2>
+const NotFound = () => {
+  const { user } = useContext(UserContext)
+  
+  return (
+    <h2 style={user.dark_mode && { color: '#EEEEEE' }}>Wups! Page Not Found...</h2>
+  )
+}
 
 export default NotFound
