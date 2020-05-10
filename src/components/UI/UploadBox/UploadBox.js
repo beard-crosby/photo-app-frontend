3fcc4c671c96b5d124292c9b4e49e02c182f3a7d
Maximilian118 <maxcrosby118@gmail.com>
2020-05-10 14:32:34 +0100

    UploadBox Compatibility check

--- src/components/UI/UploadBox/UploadBox.js
+++ src/components/UI/UploadBox/UploadBox.js
@@ -7,15 +7,24 @@ import {useDropzone} from 'react-dropzone';
 const UploadBox = () => {
   const { user, setUser } = useContext(UserContext)
 
+  const canDragDrop = () => {
+    const testDiv = document.createElement('div');
+    return (('draggable' in testDiv) || ('ondragstart' in testDiv && 'ondrop' in testDiv)) && 'FormData' in window && 'FileReader' in window;
+  }
+
   const {acceptedFiles, fileRejections, getRootProps, getInputProps, isDragActive } = useDropzone({ 
     accept: 'image/jpeg, image/png',
     multiple: false,
     maxSize: 1048576,
   })
 
-  useEffect(() => setUser({ ...user, file: acceptedFiles[0] }), [acceptedFiles])
+  useEffect(() => {
+    if (acceptedFiles.length > 0 && fileRejections.length === 0) {
+      setUser({ ...user, file: acceptedFiles[0] })
+    }
+  }, [acceptedFiles])
 
-  let text = <h1>Choose an image<br/>or drag it here</h1>
+  let text = <h1>Choose an image<br/>{canDragDrop && `or drag it here`}</h1>
 
   if (acceptedFiles.length > 0 && fileRejections.length > 0) {
     text = <h1>Multiple files<br/>Please select one image</h1>
@@ -31,12 +40,12 @@ const UploadBox = () => {
         text = <h1>File size too large<br/>10MB maximum</h1>
         break
       default:
-        text = <h1>Choose an image<br/>or drag it here</h1>
+        text = <h1>Choose an image<br/>{canDragDrop && `or drag it here`}</h1>
     }
   }
 
   return (
-    <div {...getRootProps({className: `upload-box ${isDragActive && `drag-active`}`})}>
+    <div {...getRootProps({className: `upload-box ${canDragDrop && `can-drag-drop`} ${isDragActive && `drag-active`}`})}>
       <input {...getInputProps()}/>
       <Upload/>
       {text}
