b7250caeed0070b93c9fb961a454301eb2c6bc01
Maximilian118 <maxcrosby118@gmail.com>
2020-05-16 18:44:24 +0100

    ProfileCard Sidebar Hover

--- src/components/ProfileCard/ProfileCard.js
+++ src/components/ProfileCard/ProfileCard.js
@@ -4,9 +4,10 @@ import ProfilePicture from '../UI/ProfilePicture'
 import PropTypes from 'prop-types'
 
 const ProfileCard = ({ user, style, sidebar }) => 
-  <div className={styles.profile} style={{ ...style, borderBottom: sidebar && "1px solid #EEEEEE" }}>
-    <ProfilePicture user={user} heightWidth={sidebar && 40} style={{ marginRight: sidebar ? 10 : 20 }}/>
+  <div className={`${styles.profile} ${sidebar && styles.sidebar}`} style={style}>
+    <ProfilePicture user={user} disable={sidebar} heightWidth={sidebar && 40} style={{ marginRight: sidebar ? 10 : 20 }}/>
     <div className={styles.ProfileInfo}>
+      {sidebar && <h5 className={styles.viewProfile}>View Profile</h5>}
       {sidebar ? <h5>{user.name}</h5> : <h2>{user.name}</h2>}
       {user.email && user.settings.display_email && <p>{user.email}</p>}
       {user.website && user.settings.display_website && <p>{user.website}</p>}
