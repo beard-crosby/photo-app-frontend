1aa46025394099522451da10313188aee974f322
Maximilian118 <maxcrosby118@gmail.com>
2020-05-18 16:29:48 +0100

    PhotoCard Edit Btn.

--- src/components/PhotoCard/PhotoCard.js
+++ src/components/PhotoCard/PhotoCard.js
@@ -4,21 +4,22 @@ import styles from './_PhotoCard.module.scss'
 import PropTypes from 'prop-types'
 
 const PhotoCard = ({ user, post, author }) => {
-  const [imgClicked, setImgClicked] = useState(null)
+  const [ imgClicked, setImgClicked ] = useState(false)
+  const [ edit, setEdit ] = useState(false)
 
   const imgClickedHandler = () => {
-    if (imgClicked === null) {
+    if (imgClicked === false) {
       setImgClicked(styles.imgClicked)
       document.body.style.overflow = "hidden"
     } else {
-      setImgClicked(null)
+      setImgClicked(false)
       document.body.style = "none"
     }
   }
 
   return (
-    <div className={`${styles.photoCardWrapper} ${imgClicked} ${user._id === author._id && styles.postSettings} ${user.settings.dark_mode && styles.darkMode}`}>
-      <div className={styles.imgWrapper} onClick={() => imgClickedHandler()}>
+    <div className={`${styles.photoCard} ${imgClicked} ${user._id === author._id && styles.postSettings} ${user.settings.dark_mode && styles.darkMode}`}>
+      <div className={`${styles.imgWrapper} ${edit && styles.showEdit}`} onClick={() => !edit && imgClickedHandler()}>
         <img alt="Post" src={post.img}/>
       </div>
       <div className={styles.sidebar}>
@@ -29,10 +30,10 @@ const PhotoCard = ({ user, post, author }) => {
           </div>
           {user._id !== author._id ? <input type="text" name="comment" placeholder="Write a comment" /> :
           <div className={styles.postSettings}>
-            <div className={styles.edit}>
+            <div className={styles.editBtn} onClick={() => setEdit(!edit)}>
               <h5>Edit</h5>
             </div>
-            <div className={styles.delete}>
+            <div className={styles.deleteBtn}>
               <h5>Delete</h5>
             </div>
           </div>}
