4e733aa241d954eef36a0ba357f9cb17753e279f
Maximilian118 <maxcrosby118@gmail.com>
2020-05-22 18:21:34 +0100

    PhotoCard with svgBar

--- src/components/Cards/PhotoCard/PhotoCard.js
+++ src/components/Cards/PhotoCard/PhotoCard.js
@@ -2,25 +2,40 @@ import React, { useState } from 'react'
 import ProfileCard from '../ProfileCard'
 import styles from './_PhotoCard.module.scss'
 import PropTypes from 'prop-types'
+import { Maximize2, Heart } from 'react-feather'
 
 const PhotoCard = ({ user, post, author }) => {
-  const [ imgClicked, setImgClicked ] = useState(false)
+  const [ imgClicked, setImgClicked ] = useState("undefined")
+  const [ heartClicked, setHeartClicked ] = useState("undefined")
   const [ edit, setEdit ] = useState(false)
+  const isAuthor = user._id === author._id
 
-  const imgClickedHandler = () => {
-    if (imgClicked === false) {
+  const maximiseClickedHandler = () => {
+    if (imgClicked === "undefined") {
       setImgClicked(styles.imgClicked)
       document.body.style.overflow = "hidden"
     } else {
-      setImgClicked(false)
+      setImgClicked("undefined")
       document.body.style = "none"
     }
   }
 
+  const favouriteClickedHandler = () => {
+    if (heartClicked === "undefined") {
+      setHeartClicked(styles.heartClicked)
+    } else {
+      setHeartClicked("undefined")
+    }
+  }
+
   return (
-    <div className={`${styles.photoCard} ${imgClicked} ${user._id === author._id && styles.postSettings} ${user.settings.dark_mode && styles.darkMode}`}>
-      <div className={`${styles.imgWrapper} ${edit && styles.showEdit}`} onClick={() => !edit && imgClickedHandler()}>
+    <div className={`${styles.photoCard} ${imgClicked} ${!isAuthor && styles.postSettings} ${user.settings.dark_mode && styles.darkMode}`}>
+      <div className={`${styles.imgWrapper} ${edit && styles.showEdit}`}>
         <img alt="Post" src={post.img}/>
+          {!edit && !isAuthor && <div className={styles.svgBar}>
+            <Heart className={heartClicked} onClick={() => favouriteClickedHandler()}/>
+            <Maximize2 onClick={() => maximiseClickedHandler()}/>
+          </div>}
       </div>
       <div className={styles.sidebar}>
         <div className={styles.sidebarWrapper}>
@@ -28,7 +43,7 @@ const PhotoCard = ({ user, post, author }) => {
           <div className={styles.comments}>
 
           </div>
-          {user._id !== author._id ? <input type="text" name="comment" placeholder="Write a comment" /> :
+          {!isAuthor ? <input type="text" name="comment" placeholder="Write a comment" /> :
           <div className={styles.postSettings}>
             <div className={styles.editBtn} onClick={() => setEdit(!edit)}>
               <h5>Edit</h5>
