5874fdd539ecf64013800aa56fcc17bb214985c8
Maximilian118 <maxcrosby118@gmail.com>
2020-05-22 16:32:18 +0100

    Profile SVG's

--- src/components/UserInfo/UserInfo.js
+++ src/components/UserInfo/UserInfo.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import styles from './_UserInfo.module.scss'
+import { MoreHorizontal, Edit2 } from 'react-feather'
 
 const UserInfo = () => 
   <div className={styles.userInfoWrapper}>
     <div className={styles.top}>
       <h5>ABOUT</h5>
+      <MoreHorizontal/>
     </div>
     <div className={styles.userInfo}>
-
+    <Edit2/>
     </div>
   </div>
 
