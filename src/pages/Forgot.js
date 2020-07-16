4b71acf2a47e832cad9a3a8f89a9759f5ffaf93c
Maximilian118 <maxcrosby118@gmail.com>
2020-07-16 15:05:57 +0200

    MasonryWrapper on forgot

--- src/pages/Forgot.js
+++ src/pages/Forgot.js
@@ -6,6 +6,8 @@ import '../components/UI/FormSection'
 import { checkFormValid } from '../shared/formValidation'
 import FormSection from '../components/UI/FormSection'
 import { Mail } from 'react-feather'
+import MasonryWrapper from '../components/Masonry/MasonryWrapper'
+import { posts } from '../shared/postRequests'
 
 const Forgot = ({ history }) => {
   const { user, setUser } =  useContext(Context)
@@ -24,22 +26,26 @@ const Forgot = ({ history }) => {
     return () => user.formErrors && setUser(removeKey(user, "formErrors"))
   }, [user, setUser, form])
 
+  useEffect(() => posts(user, setUser, 20, 4), []) // eslint-disable-line react-hooks/exhaustive-deps
+
   const onSubmit = event => {
     event.preventDefault()
     // backend request
   }
 
   return (
-    <form className="model" onSubmit={event => onSubmit(event)}>
-      <div className="top">
-        <h5 className="title">FORGOT PASSWORD</h5>
-        <h5 onClick={() => history.goBack()}>BACK</h5>
-      </div>
-      <div className="middle">
-        <FormSection text={"Email"} err={form.errors.emailError} user={user} form={form} setForm={setForm}/>
-        <Button text="Submit" submit disabled={!formValid} icon={<Mail/>}/>
-      </div>
-    </form>
+    <MasonryWrapper user={user}>
+      <form className="model" onSubmit={event => onSubmit(event)} style={{ width: 343, marginBottom: 7 }}>
+        <div className="top">
+          <h5 className="title">FORGOT PASSWORD</h5>
+          <h5 onClick={() => history.goBack()}>BACK</h5>
+        </div>
+        <div className="middle">
+          <FormSection text={"Email"} err={form.errors.emailError} user={user} form={form} setForm={setForm}/>
+          <Button text="Submit" submit disabled={!formValid} icon={<Mail/>}/>
+        </div>
+      </form>
+    </MasonryWrapper>
   )
 }
 
