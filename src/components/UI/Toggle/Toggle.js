bcc266ecc11c49d3cb87e7be3232d673d5b5458a
Maximilian118 <maxcrosby118@gmail.com>
2020-07-19 12:04:15 +0200

    Slider changes on Toggle default change

--- src/components/UI/Toggle/Toggle.js
+++ src/components/UI/Toggle/Toggle.js
@@ -1,10 +1,12 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import styles from './_Toggle.module.scss'
 import Slider from './Slider/Slider'
 import PropTypes from 'prop-types'
 
 const Toggle = ({ text, Default, onClick }) => {
   const [ slide, setSlide ] = useState(Default)
+
+  useEffect(() => setSlide(Default), [Default])
 
   const onClickHandler = () => {
     setSlide(!slide)
